f0ef9374c76184e9f325e1be7e36dd1dc809c4d7
Christian Schweigel <christian.david.schweigel@sap.com>
2019-02-07 08:35:00 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='LunaDB',
+      version='0.1.2',
+      description='Easy file based NoSQL DB',
+      author='Christian Schweigel',
+      author_email='',
+      url='',
+      packages=['LunaDB', 'LunaDB.exceptions'],
+     )
