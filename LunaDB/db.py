350227a2e1143b8c964672a617ce9df3e3e1a629
Christian Schweigel <christian.david.schweigel@sap.com>
2019-03-07 16:08:30 +0100

    Root table methods

--- LunaDB/db.py
+++ LunaDB/db.py
@@ -7,7 +7,7 @@ class LunaDB():
         self.path = name + "/"
         if not os.path.isdir(self.path):
             os.mkdir(self.path)
-            open(self.path + "#", "w").close()
+        self.root_table = self.table("#")
         
     def table(self, name, id_field = None):
         return Table(self.path, name, id_field = id_field)
@@ -15,4 +15,21 @@ class LunaDB():
     def drop_table(self, name):
         del_path = self.path + name
         os.remove(del_path)
+
+    ###### Root table link methods
+    
+    def insert(self, row, strict = True):
+        self.root_table.insert(row, strict)
+
+    def insert_multiple(self, rows, strict = True):
+        self.root_table.insert_multiple(rows, strict)
+    
+    def delete(self, filter_function, auto_clean = True):
+        self.root_table.delete(filter_function, auto_clean)
+    
+    def search(self, filter_function):
+        return self.root_table.search(filter_function)
+
+    
+
     
