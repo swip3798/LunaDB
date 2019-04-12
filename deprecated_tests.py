5265373b5f74f3503994c491c71ebd5b2a7ebd12
Christian Schweigel <christian.david.schweigel@sap.com>
2019-04-12 13:38:56 +0200

    Delete deprecated tests

--- deprecated_tests.py
+++
@@ -1,9 +0,0 @@
-from LunaDB import LunaDB
-
-db = LunaDB("test", auto_clean_buffer=10000)
-table = db.table("delete_test")
-
-
-table.insert({"iojsdoija":"jdaojdw"})
-table.insert({"saoijdiowj":2})
-table.delete(lambda x: x["_id"] == 0)
